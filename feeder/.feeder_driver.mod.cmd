fb3ace933c7d1c5c8dd24e45878e84b4abd31679
MIN60 <whitedaisy12759@gmail.com>
2025-07-01 20:21:38 +0100

    Del/#2: Clean up files in kernel module

--- feeder/.feeder_driver.mod.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/veda/factory/feeder/feeder_driver.mod := printf '%s\n'   feeder_driver.o | awk '!x[$$0]++ { print("/home/veda/factory/feeder/"$$0) }' > /home/veda/factory/feeder/feeder_driver.mod
--- feeder/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/veda/factory/feeder/modules.order := {   echo /home/veda/factory/feeder/feeder_driver.o; :; } > /home/veda/factory/feeder/modules.order
